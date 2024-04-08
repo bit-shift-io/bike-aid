a7ed208ef9c29d11c620db5ddce193638bd56ae1
Bronson <bronsonmathews@gmail.com>
2024-04-08 21:44:06 +0930

    rough gui

---
+++ bike-aid-app/src/lib.rs
@@ -0,0 +1,23 @@
+slint::include_modules!();
+
+pub fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.run()
+}
+
+
+#[cfg(target_os = "android")]
+#[no_mangle]
+fn android_main(app: slint::android::AndroidApp) {
+    slint::android::init(app).unwrap();
+    main();
+}
--- bike-aid-app/src/main.rs
+++ bike-aid-app/src/main.rs
@@ -1,15 +1,3 @@
-slint::include_modules!();
-
-fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
-
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
-
-    ui.run()
+pub fn main() {
+    bike_aid::main();
 }
