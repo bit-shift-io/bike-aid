8514742e1b66978b13e75aecca6402f2437748b5
Bronson <bronsonmathews@gmail.com>
2024-04-10 22:32:17 +0930

    gui

--- bike-aid-app/src/lib.rs
+++ bike-aid-app/src/lib.rs
@@ -1,17 +1,8 @@
 slint::include_modules!();
 
-pub fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
-
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
-
-    ui.run()
+pub fn main() {
+    let window = AppWindow::new().unwrap();
+    window.run().unwrap();
 }
 
 
