ce80a0e3729a11dafd12aadce4d7750f9cac5aef
Bronson <bronsonmathews@gmail.com>
2024-04-11 00:02:29 +0930

    bluetooth test

--- bike-aid-app/src/lib.rs
+++ bike-aid-app/src/lib.rs
@@ -1,7 +1,10 @@
 slint::include_modules!();
+mod bluetooth;
+
 
 pub fn main() {
     let window = AppWindow::new().unwrap();
+    window.on_scan(do_scan);
     window.run().unwrap();
 }
 
@@ -12,3 +15,10 @@ fn android_main(app: slint::android::AndroidApp) {
     slint::android::init(app).unwrap();
     main();
 }
+
+
+fn do_scan() {
+    println!("here");
+    bluetooth::main();
+}
+
