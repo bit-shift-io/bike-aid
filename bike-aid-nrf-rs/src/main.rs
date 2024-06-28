29fc1361454e7cb809dbb79b63ef2a9dd001bf49
Bronson <bronsonmathews@gmail.com>
2024-06-29 09:19:20 +0930

    twi

--- bike-aid-nrf-rs/src/main.rs
+++ bike-aid-nrf-rs/src/main.rs
@@ -15,7 +15,6 @@ P0.31 - TWI SCL
 // modules/creates
 mod signals;
 mod task_clock;
-mod task_twi_manager;
 mod task_led;
 mod task_temperature;
 mod task_speed;
@@ -38,6 +37,36 @@ async fn main(spawner: Spawner) {
 
     // DEBUG: add sleep incase we need to flash during debug and get a crash
     Timer::after_secs(2).await;
+
+    // Configure and setup shared async I2C/TWI communication
+    let mut shared_i2c = {
+        use embassy_nrf::{bind_interrupts, peripherals::{self}, twim::{self, Twim}};
+        bind_interrupts!(struct Irqs {
+            SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0 => twim::InterruptHandler<peripherals::TWISPI0>;
+        });
+        let port_twi = p.TWISPI0;
+        let pin_sda = p.P0_29.degrade();
+        let pin_scl = p.P0_31.degrade();
+        let config = twim::Config::default();
+        Twim::new(port_twi, Irqs, pin_sda, pin_scl, config)
+        //SHARED_ASYNC_I2C.init(Mutex::new(i2c))
+    };
+
+    // scan for i2c/twi devices
+    for address in 1..128 {
+        match shared_i2c.write(address, &[]).await {
+            Ok(_) => {
+                info!("Device found at address: 0x{:X}", address);
+            }
+            Err(_) => continue,
+        }
+    }
+
+    // INIT DEVICES
+
+    
+
+    // INIT TASKS
 
     // Clock Task
     use crate::task_clock::clock;
@@ -75,26 +104,10 @@ async fn main(spawner: Spawner) {
     use crate::task_bluetooth::bluetooth;
     spawner.must_spawn(bluetooth());
  
-    // TWI
-    use crate::task_twi_manager::twi_manager;
-    spawner.must_spawn(twi_manager(
-        p.TWISPI0,
-        p.P0_29.degrade(),
-        p.P0_31.degrade()
-    ));
-    
-
-    // todo: flash led boot complete
-    let mut pub_led = signals::LED_MODE.publisher().unwrap();
-    /*
-    pub_led.publish_immediate(task_led::LedMode::ThreeFast);
-    let _ = embassy_futures::poll_once(async {
-        pub_led.publish(task_led::LedMode::ThreeFast).await;
-    });
-     */
     
 
     // loop for testing
+    let pub_led = signals::LED_MODE.publisher().unwrap();
     let mut sub_minutes = signals::CLOCK_MINUTES.subscriber().unwrap();
     loop {
         let val = sub_minutes.next_message_pure().await;
