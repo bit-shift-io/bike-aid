eb44aff62062f6817b0fdbdd74a98dcca61a0dce
Bronson <bronsonmathews@gmail.com>
2024-07-26 22:28:08 +0930

    working app

--- bike-aid-app/README.md
+++ bike-aid-app/README.md
@@ -7,10 +7,81 @@ A Rust application that's using [Slint](https://slint.rs) for the user interface
 Using bluetooth low energy (BLE) and desktop and mobile app.
 
 ## VSCode plugins
-Slint
-rust-analyzer
-CodeLLDB
+Slint  
+rust-analyzer  
+CodeLLDB  
 
 ## Getting rust working
 Run ```cargo build``` in the rust project root directory  
 Then ```cargo run```
+
+## Setup Android
+Install sdk & ndk in the ~/Android/ directory
+Install dependencies
+```bash
+sudo pacman -S jdk11-openjdk clang lld llvm
+```
+
+Set env variables in ~/.bashrc
+```bash
+export ANDROID_HOME=$HOME/Android/Sdk
+export ANDROID_SDK_ROOT=$ANDROID_HOME
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+```
+
+Run ```sudo adb devices```
+
+
+## wireless adb
+Enable wireless debugging on android
+Click it and get the ip, port and pair code.
+```sudo adb pair ip:port```
+insert paircode.
+```sudo adb connect ip:port```
+
+## run android
+```bash
+x doctor
+x devices
+x build
+x build --device adb:<id>
+x run --device adb:<id>
+```
+
+
+
+## Links
+
+#### BLE
+
+https://github.com/MnlPhlp/blec
+
+
+#### Logging
+
+https://docs.rs/android_logger/latest/android_logger/
+
+https://tms-dev-blog.com/log-to-a-file-in-rust-with-log4rs/
+
+
+#### Android
+https://github.com/slint-ui/slint/tree/3aafce2c52fa1e14971e4f2455bdc556daeb079d/internal/backends/android-activity
+
+https://github.com/slint-ui/slint/blob/master/examples/todo/rust/lib.rs
+
+https://git.sr.ht/~tmpod/eigen/tree/main/item/src
+
+https://github.com/search?q=repo%3Aslint-ui%2Fslint%20android&type=code
+
+https://github.com/slint-ui/slint/blob/ed7eda02860ce9d0256268d034913aeece8e5f81/api/rs/slint/android.rs#L93
+
+https://crates.io/crates/android-activity
+
+https://docs.rs/android-manifest/latest/android_manifest/struct.UsesPermission.html
+
+#### JNI
+
+https://docs.rs/jni/latest/jni/
+
+https://github.com/astonbitecode/j4rs
--- bike-aid-app/src/lib.rs
+++ bike-aid-app/src/lib.rs
@@ -1,22 +1,29 @@
+// log for android
+#[macro_use] extern crate log;
+
+
 slint::include_modules!();
+
+/*
 use blec::{ble, BleAddress};
 use uuid::Uuid;
 mod bluetooth;
-
+ */
 // reference here:
-// https://github.com/MnlPhlp/blec
+// 
+
 
 pub fn init() {
-    // init blec bluetooth
-    let result = ble::init();
-    match result {
-        Ok(_) => {
-            println!("Bluetooth initialized");
-        }
-        Err(_) => {
-            println!("Bluetooth not initialized");
-        }
-    }
+    // // init blec bluetooth
+    // let result = ble::init();
+    // match result {
+    //     Ok(_) => {
+    //         println!("Bluetooth initialized");
+    //     }
+    //     Err(_) => {
+    //         println!("Bluetooth not initialized");
+    //     }
+    // }
 }
 
 
@@ -26,78 +33,87 @@ pub async fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
     let ui_weak = ui.as_weak();
 
-    /*
+    // /*
+    // ui.on_connect(move || {
+    //     let my_ui = ui_weak.unwrap();
+    //     // ....
+    //     let _ = slint::spawn_local(async move {
+    //         // ...
+    //         let foobar = tokio::task::spawn(async move {
+    //              // do the thing that needs to run on a tokio executor
+    //              let foobar = ble::connect(
+    //                 BleAddress::from_str_delim("00:11:22:33:44:55").unwrap(),
+    //                 Uuid::parse_str("a1a2a3a4b1b2c1c2d1d2d3d4d5d6d7d8").unwrap(),
+    //                 Vec<Uuid::parse_str("a1a2a3a4b1b2c1c2d1d2d3d4d5d6d7d8").unwrap()>,
+    //                 {},
+    //              ).await;
+    //              foobar
+    //         }).await;
+    //         // now use foobar to set some property
+    //         // ...
+    //         my_ui.set_speed(15);
+    //         });
+    // });
+    //  */
+
+
+    // async tasks
     ui.on_connect(move || {
         let my_ui = ui_weak.unwrap();
-        // ....
+
         let _ = slint::spawn_local(async move {
-            // ...
             let foobar = tokio::task::spawn(async move {
                  // do the thing that needs to run on a tokio executor
-                 let foobar = ble::connect(
-                    BleAddress::from_str_delim("00:11:22:33:44:55").unwrap(),
-                    Uuid::parse_str("a1a2a3a4b1b2c1c2d1d2d3d4d5d6d7d8").unwrap(),
-                    Vec<Uuid::parse_str("a1a2a3a4b1b2c1c2d1d2d3d4d5d6d7d8").unwrap()>,
-                    {},
-                 ).await;
+                 //let foobar = bluetooth::scan_stream().await;
+                 let foobar = 48;
                  foobar
             }).await;
+
             // now use foobar to set some property
-            // ...
-            my_ui.set_speed(15);
-            });
+            my_ui.set_speed(foobar.unwrap());
+        });
     });
-     */
-
-
-     /*
-    ui.on_connect(move || {
-        let my_ui = ui_weak.unwrap();
-        // ....
-        let _ = slint::spawn_local(async move {
-            // ...
-            let foobar = tokio::task::spawn(async move {
-                 // do the thing that needs to run on a tokio executor
-                 let foobar = bluetooth::scan_stream().await;
-                 foobar
-            }).await;
-            // now use foobar to set some property
-            // ...
-            my_ui.set_speed(15);
-            });
-    }); */
 
    
-    /*
-    // this works for calling
-    ui.on_scan({
-        move || {
-            // Spawn thread to be able to call async functions.
-            tokio::spawn(async move {
+    // /*
+    // // this works for calling
+    // ui.on_scan({
+    //     move || {
+    //         // Spawn thread to be able to call async functions.
+    //         tokio::spawn(async move {
 
-                    // Call async function
-                    println!("here");
-                    let item = bluetooth::scan_sleep().await;
-                    /* 
-                    // Update UI model state
-                    update_model(
-                        handle_weak.clone(),
-                        item,
-                    );*/
-            });
-        }
-    });
-     */
+    //                 // Call async function
+    //                 println!("here");
+    //                 let item = bluetooth::scan_sleep().await;
+    //                 /* 
+    //                 // Update UI model state
+    //                 update_model(
+    //                     handle_weak.clone(),
+    //                     item,
+    //                 );*/
+    //         });
+    //     }
+    // });
+    //  */
 
-    // non async example
+    // non async tasks
     ui.on_request_increase_value({
         let ui_handle = ui.as_weak();
         move || {
             let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
+            ui.set_speed(ui.get_speed() + 1);
         }
     });
 
+    ui.on_request_decrease_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_speed(ui.get_speed() - 1);
+        }
+    });
+
+    // run slint ui
     ui.run()
 }
 
@@ -105,7 +121,21 @@ pub async fn main() -> Result<(), slint::PlatformError> {
 #[cfg(target_os = "android")]
 #[no_mangle]
 fn android_main(app: slint::android::AndroidApp) {
+    // configure logcat
+    extern crate android_logger;
+    use log::LevelFilter;
+    use android_logger::Config;
+
+    android_logger::init_once(
+        Config::default().with_max_level(LevelFilter::Info), // change log level here
+    );
+
+    info!("==== BIKE AID START ====");
+
+    // init other
     slint::android::init(app).unwrap();
     init();
-    main();
+
+    // // main
+    let _ = main(); // can check result if we get crashes
 }
