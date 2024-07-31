7431884048f594a6b8ebbad5cc11d734503f5ac5
Bronson <bronsonmathews@gmail.com>
2024-07-31 23:45:57 +0930

    app is reading ble data

--- app-java/app/src/main/java/com/bitshift/bike_aid/BLE.java
+++ app-java/app/src/main/java/com/bitshift/bike_aid/BLE.java
@@ -5,6 +5,7 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
@@ -12,9 +13,6 @@ import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.os.Handler;
-import android.os.Looper;
-import android.os.ParcelUuid;
-import android.util.Log;
 
 import java.util.List;
 import java.util.Set;
@@ -42,7 +40,7 @@ public class BLE {
     BluetoothDevice mDevice;
     boolean deviceFound = false;
     private Context context;
-
+    protected static final UUID CHARACTERISTIC_UPDATE_NOTIFICATION_DESCRIPTOR_UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
 
     // ==== callbacks ====
     private ScanCallback scanCallback =
@@ -77,7 +75,18 @@ public class BLE {
         context = c;
         BluetoothManager bluetoothManager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
+
+        // turn on bluetooth if disabled
+        if (!bluetoothAdapter.isEnabled())
+            bluetoothAdapter.enable();
+
         bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+
+        if (bluetoothLeScanner == null) {
+            log.info("BLE: no bluetooth adapter available, please enable bluetooth");
+            return;
+        }
+
         Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
 
         // check for bonded devices
@@ -111,84 +120,66 @@ public class BLE {
 
         @Override
         public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, byte[] value) {
-            log.info("BLE characteristic changed");
-
+            // we were notified, now read the value
+            gatt.readCharacteristic(characteristic);
         }
 
         @Override
         public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, byte[] value, int status) {
-            log.info("BLE characteristic read");
+            UUID id = characteristic.getUuid();
+            String val = characteristic.getStringValue(0);
+            log.info("BLE: id " + id);
+            log.info("BLE: read " + val);
         }
 
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
             log.info("BLE service discovered");
 
-            // this is for debug only
-            // in reality we will specify uuid's and ask notify them ourselves
+            // find all characteristics
+            // then try to set notification on each one
             if (status == BluetoothGatt.GATT_SUCCESS) {
                 List<BluetoothGattService> services = gatt.getServices(); // get list of all services
-                //gatt.getService(UUID uuid); // get service by uuid
-                log.info("BLE services:");
+                log.info("BLE service scan begin...");
+
                 for (BluetoothGattService s: services) {
-                    log.info(s.getUuid().toString());
                     List<BluetoothGattCharacteristic> characteristics = s.getCharacteristics();
 
-                    log.info("characteristics:");
                     for (BluetoothGattCharacteristic c : characteristics) {
+                        setCharacteristicNotification(
+                                gatt,
+                                s.getUuid(),  // service
+                                c.getUuid(),  // characteristic
+                                true);
                         log.info(c.getUuid().toString());
                     }
                     log.info("");
                 }
+
+                log.info("BLE: service scan complete");
             }
-
-
-            // TODO: move to function
-            // gatt services
-
-
-            // uart
-            BluetoothGattService s_uart = gatt.getService(UUID.fromString("6E400001-B5A3-F393-E0A9-E50E24DCCA9E"));
-            BluetoothGattCharacteristic c_rx = s_uart.getCharacteristic(UUID.fromString("6E400002-B5A3-F393-E0A9-E50E24DCCA9E"));
-            BluetoothGattCharacteristic c_tx = s_uart.getCharacteristic(UUID.fromString("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"));
-            boolean res1 = gatt.setCharacteristicNotification(c_rx, true); // no notify on rx, use this for writing
-            boolean res2 = gatt.setCharacteristicNotification(c_tx, true); // use for reading
-            log.info("BLE notify: rx:" + res1 + " tx:" + res2);
-
-
-            // data
-            BluetoothGattService s_data = gatt.getService(uuidFrom16bit("2000"));
-            // get characteristics
-            // data
-            BluetoothGattCharacteristic c_speed = s_data.getCharacteristic(uuidFrom16bit("2001"));
-            BluetoothGattCharacteristic c_trip_duration = s_data.getCharacteristic(uuidFrom16bit("2002"));
-            BluetoothGattCharacteristic c_odometer = s_data.getCharacteristic(uuidFrom16bit("2003"));
-            BluetoothGattCharacteristic c_temperature = s_data.getCharacteristic(uuidFrom16bit("2004"));
-            BluetoothGattCharacteristic c_clock_minutes = s_data.getCharacteristic(uuidFrom16bit("2005"));
-            BluetoothGattCharacteristic c_clock_hours = s_data.getCharacteristic(uuidFrom16bit("2006"));
-
-            // set notify
-            // data
-            gatt.setCharacteristicNotification(c_speed, true);
-            gatt.setCharacteristicNotification(c_trip_duration, true);
-            gatt.setCharacteristicNotification(c_odometer, true);
-            gatt.setCharacteristicNotification(c_temperature, true);
-            gatt.setCharacteristicNotification(c_clock_minutes, true);
-            gatt.setCharacteristicNotification(c_clock_hours, true);
-
-
-            // settings
-            BluetoothGattService s_settings = gatt.getService(uuidFrom16bit("1000"));
-
-
 
         }
 
     };
 
+
+    // setup notifications
+    public boolean setCharacteristicNotification(BluetoothGatt gatt, UUID serviceUuid, UUID characteristicUuid, boolean enable) {
+        BluetoothGattCharacteristic characteristic = gatt.getService(serviceUuid).getCharacteristic(characteristicUuid);
+        gatt.setCharacteristicNotification(characteristic, enable);
+        BluetoothGattDescriptor descriptor = characteristic.getDescriptor(CHARACTERISTIC_UPDATE_NOTIFICATION_DESCRIPTOR_UUID);
+        if (descriptor == null)
+            return false;
+        byte[] val = enable ? BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE : BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE;
+        descriptor.setValue(val);
+        return gatt.writeDescriptor(descriptor); //descriptor write operation successfully started?
+    }
+
+
     // Converts 16bit UUIDs to 128-bit format
     // the 16bit uuid is short for 0000xxxx-0000-1000-8000-00805F9B34FB
-    public static UUID uuidFrom16bit(String uuid16) {
+    public static UUID uuidFrom16(String uuid16) {
         String baseUUIDSuffix = "0000-1000-8000-00805F9B34FB";
         String uuid = "0000" + uuid16 + baseUUIDSuffix;
         return UUID.fromString(uuid);
